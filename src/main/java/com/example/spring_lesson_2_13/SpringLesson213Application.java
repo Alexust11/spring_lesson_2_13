c77dac8f7d25b0578a151681ea72fe022915c496
User <alexust11@yandex.ru>
2022-11-27 21:50:03 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_lesson_2_13/SpringLesson213Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_lesson_2_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson213Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLesson213Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_lesson_2_13/SpringLesson213ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_lesson_2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson213ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
